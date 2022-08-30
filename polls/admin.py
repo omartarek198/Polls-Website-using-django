7d0476019cfe59505bb32c4bd14fa3f38cc56225
Omar <omar.tarek29@msa.edu.eg>
2022-08-30 20:46:21 +0200

    added admin configuration

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Choice, Question
+
+
+# admin.site.register(Question)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None,{'fields': ['question']})]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question,QuestionAdmin)
+
 
 # Register your models here.
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,18 @@
+from pyexpat import model
+from secrets import choice
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+
 class Question(models.Model):
     question = models.CharField(max_length=300)
 
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=280)
+
+    def __str__(self):
+        return self.choice
